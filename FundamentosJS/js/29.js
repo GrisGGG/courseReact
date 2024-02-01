7db6fb0227f17bfb5007f46c676cd9269ede6c47
GrisGGG <astropoetscapicua@gmail.com>
2024-01-31 21:53:35 -0600

    Fundamentos de Javascript

---
+++ FundamentosJS/js/29.js
@@ -0,0 +1,13 @@
+//template strings
+const producto = "Tablet de 12 pulgadas";
+const precio = 400;
+const marca = "Orange";
+
+function textoDeFuncion() {
+  return "Este texto proviene de la función";
+}
+
+console.log(producto + " $" + precio + " Dolares, Marca: " + marca);
+console.log(
+  `${producto} $${precio} Dolares, Marca: ${marca} ${textoDeFuncion()}`
+);
