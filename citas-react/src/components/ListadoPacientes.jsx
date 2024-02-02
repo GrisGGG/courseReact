c5f5503e30be7a5aa75e6d1d0094ca1b094b810d
GrisGGG <astropoetscapicua@gmail.com>
2024-02-02 08:15:24 -0600

    Agregando  y estilizando componentes

--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -1,7 +1,17 @@
+import Paciente from "./Paciente"
 function ListadoPacientes() {
   return (
-    <div className="w-1/2 lg:w-3/5">
-      <h3>Listado</h3>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">
+        Listado de Pacientes
+      </h2>
+      <p className="text-lg mt-5 text-center">
+        Adimistra tus {''}
+        <span className="text-indigo-600 bold">
+          Pacientes y Citas</span>
+      </p>
+    <Paciente/>
+      
     </div>
   )
 }
---
+++ citas-react/src/components/Paciente.jsx
@@ -0,0 +1,23 @@
+const Paciente = () => {
+  return (
+    <div className="ml-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal -case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+          <span className="font-normal normal -case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+          <span className="font-normal normal -case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de Alta: {''}
+          <span className="font-normal normal -case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+          <span className="font-normal normal -case">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab magnam doloribus atque exercitationem nam explicabo libero fugit eligendi officia dicta magni, dolor porro delectus architecto sapiente voluptatem pariatur ut quae.</span>
+        </p>   
+      </div>
+  )
+}
+
+export default Paciente
