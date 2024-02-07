1f391e752dc003673e7132f27850482556f20be0
GrisGGG <astropoetscapicua@gmail.com>
2024-02-06 22:12:24 -0600

    Se añade texto condicional al componente de listado de pacientes

--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -1,29 +1,39 @@
 import Paciente from "./Paciente"
 function ListadoPacientes({pacientes}) {
   
- 
+ console.log(pacientes.length);
   return (
+
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">
-        Listado de Pacientes
-      </h2>
-      <p className="text-lg mt-5 text-center">
-        Adimistra tus {''}
-        <span className="text-indigo-600 bold">
-          Pacientes y Citas</span>
-      </p>
-    {
-       pacientes.map(paciente =>(
-        <Paciente 
-        key = {paciente.id}
-         paciente = {paciente}/>
+{/* texto condicional si hay pacientes o no */}
+      {pacientes && pacientes.length ? 
+      (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+          <p className="text-lg mt-5 text-center">Adimistra tus {''}
+          <span className="text-indigo-600 bold">Pacientes y Citas</span>
+          </p>
+          {
+          pacientes.map(paciente =>(
+          <Paciente 
+          key = {paciente.id}
+          paciente = {paciente}/>
       ))
-    
-      
-    }
-      
+          }
+        </>
+      )
+      :
+      (
+        <>
+        <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+        <p className="text-lg mt-5 text-center">Comienza agregando pacientes {''}
+        <span className="text-indigo-600 bold">
+        y aparecerán aquí.</span>
+        </p>
+        </>
+      )}  
     </div>
   )
 }
 
-export default ListadoPacientes
+export default ListadoPacientes;
