b27298e9c832b5f1ef3fc311ef5e2c0f6ff6d335
GrisGGG <astropoetscapicua@gmail.com>
2024-04-03 23:04:02 -0600

    Añadiendo función addToCart para añadir productos al carrito

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -6,7 +6,11 @@ import { useState } from "react"
 function App() {
     //state
     const [data, setData] = useState(db);
-    
+    const [cart, setCart] = useState([]);
+
+    function addToCart(item){
+       setCart(prevCart => [...prevCart, item])
+    }
 
   return (
    <>
@@ -18,7 +22,9 @@ function App() {
         <div className="row mt-5">
             {data.map((guitar)=>(
             <Guitar
+            key={guitar.id}
             guitar={guitar}
+            addToCart={addToCart}
             />   
             ))    
 }        </div>
